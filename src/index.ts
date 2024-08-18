80a6c1fe7d555ca6fa7be40d1d51bbad8a9b7068
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2024-08-18 18:30:17 +0530

    feat: add gql context

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { expressMiddleware } from "@apollo/server/express4";
-import { createApolloGraphqlServer } from './graphql/index';
+import UserService from "./services/user";
+import createApolloGraphqlServer from "./graphql/index";
 async function init() {
   const app = express();
   const PORT = Number(process.env.PORT) || 8000;
@@ -10,7 +11,22 @@ async function init() {
   app.get("/", (req, res) => {
     res.json({ message: "Server is up and running" });
   });
-  app.use("/graphql", expressMiddleware(await createApolloGraphqlServer()));
+  app.use(
+    "/graphql",
+    expressMiddleware(await createApolloGraphqlServer(), {
+      context: async ({ req }) => {
+        const token =
+          req.headers?.authorization?.split("Bearer ")[1] || undefined;
+
+        try {
+          const user = UserService.decodeJWTToken(token as string);
+          return { user };
+        } catch (error) {
+          return {};
+        }
+      },
+    })
+  );
 
   app.listen(PORT, () => console.log(`Server started at : ${PORT}`));
 }
