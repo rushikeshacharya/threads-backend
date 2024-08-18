d2f3813947cdc45e9d7b4bb7edd14a710b4a2a27
Rushikesh Acharya <rushikeshacharya09@gmail.com>
2024-08-18 18:30:58 +0530

    feat: add new query

--- src/graphql/user/resolvers.ts
+++ src/graphql/user/resolvers.ts
@@ -1,7 +1,27 @@
-const queries = {};
+import UserService, { CreateUserPayload } from "../../services/user";
+
+const queries = {
+  getUserToken: async (
+    _: any,
+    payload: { email: string; password: string }
+  ) => {
+    const token = await UserService.getUserToken({
+      email: payload.email,
+      password: payload.password,
+    });
+    return token;
+  },
+  getCurrentLoggedInUser: async (_: any, parameters: any, context: any) => {
+    if (context && context.user) {
+      const user = await UserService.getUserById(context.user.id);
+      return user;
+    }
+    throw new Error("User is not logged in");
+  },
+};
 const mutations = {
-  createUser: async (_: any, {}: {}) => {
-    return "12345678";
+  createUser: async (_: any, payload: CreateUserPayload) => {
+    return (await UserService.createUser(payload)).id;
   },
 };
 
